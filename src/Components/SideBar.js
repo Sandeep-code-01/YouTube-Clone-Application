8cb75a1a691c62ceeb09dc4c8a424271d4fe3f3b
Sandeepkumarchauhan <106464057+Sandeepkumarchauhan@users.noreply.github.com>
2026-01-26 15:03:07 +0530

    header with youtube logo

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-gray-900">SideBar</h1>
+    <div className="p-4 w-60 border-r border-gray-300 h-screen">
+      <h1> Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>News</li>
+        <li>Gaming</li>
+        <li>Education</li>  
+      </ul>
     </div>
   )
 }
