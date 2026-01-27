23e7869a79131f09e1ff8d52874c1292255e4939
Sandeepkumarchauhan <106464057+Sandeepkumarchauhan@users.noreply.github.com>
2026-01-27 10:11:21 +0530

    updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,21 +16,22 @@ const Header = () => {
       <div className="flex items-center gap-3">
         <button
           onClick={handleToggleMenu}
-          className="p-2 rounded-full hover:bg-gray-200 active:scale-95 transition-all duration-200"
+          className="p-2 rounded-full hover:bg-gray-100 active:scale-95 transition-all duration-200"
           aria-label="Toggle Menu"
         >
           <img
             className="h-6 w-6"
             alt="Menu"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnMg7VcAc6WkebF912z4ABmiZPlt1Y_TpXbg&s"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAKMAAACUCAMAAADIzWmnAAAAVFBMVEX///8jHyAAAAAgHB21s7QwLC3LysrOzs4MAATr6+v8/PzY2NjT09PDw8M9Ojq8vLyEgoJCP0Dh4eFMSUocFxiLiYnx8fF4dnY0MTITDQ5nZWZRT09a1d6FAAAAwklEQVR4nO3bW27CMBBA0eBQSoz7IuHRsv99tmvoyFEGnbOCK1vJhzUzDAAAAAAAZNX23b3VUOH0XlawfAQS62fZreD76/r/xvO8rNG4Ky3Q+Nh+Y72UZeyvzIG7Hurt/tLd/ecUSPzTDt21yCkCAAAA8HSup9fu2hRKnG7HFTwOgcR6KWP/V9zxOAdOMsV7eILGDHed4ZtJ8e8BAAAA4Nlsf0YzwaxrhpnhDLPXKd7DE+wCZNipGDLspgAAAAAAsAW/dXglnXjoQbMAAAAASUVORK5CYII="
           />
         </button>
-
+        <a href="/">
         <img
           className="h-10 cursor-pointer"
           alt="YouTube Logo"
           src="https://1000logos.net/wp-content/uploads/2017/05/Youtube-Logo-2017-2048x1152.png"
         />
+        </a> 
       </div>
 
       {/* Search Section */}
@@ -48,7 +49,7 @@ const Header = () => {
       {/* Profile Section */}
       <div className="flex justify-end items-center gap-4">
         <img
-          className="h-8 w-8 cursor-pointer"
+          className="h-8 w-8 rounded-full hover:bg-gray-400 shadow cursor-pointer"
           alt="Bell Icon"
           src="https://www.bing.com/th/id/OIP.ok0xXCChzb0Ti3hSC8BH-QHaHa?w=195&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
         />
